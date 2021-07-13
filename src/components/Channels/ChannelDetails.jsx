d9f628bdd6687e0db3438c409a0e62ae3f29831b
Oliver Nero <oliver_nero11@icloud.com>
2021-07-13 19:18:48 +0800

    update-backup

--- src/components/Channels/ChannelDetails.jsx
+++ src/components/Channels/ChannelDetails.jsx
@@ -9,16 +9,15 @@ const ChannelDetails = () => {
   const { headers } = useContext(UserContext);
   const { data: channelDetails, isPending, error } = useFetchGet(
     `http://206.189.91.54//api/v1/channels/${id}`, headers)
-    console.log(channelDetails)
   return (
     <>
-      <h2>Channel Details</h2>
       <AddMember />
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      { channelDetails && 
+      { !isPending && 
       <div>
         <h4>{ channelDetails.data.name }</h4>
+        <h4>{ channelDetails.data.channel_members.length}</h4>
       </div> 
       }
     </>
--- src/routes/Client/Client.jsx
+++ src/routes/Client/Client.jsx
@@ -1,43 +1,21 @@
 import './Client.scss'
 import { Switch, Route } from "react-router-dom"
-import Channels from "../../components/Channels/Channels";
-import Messages from "../../components/Messages/Messages";
 import ChannelDetails from '../../components/Channels/ChannelDetails';
-import Logout from "../../components/Logout/Logout";
+import ContactDetails from '../../components/Messages/ContactDetails'
+import Menu from '../../components/Menu/Menu';
+
+
 
 const Client = () => {
   return (
     <div className='Client'>
-      <div className="side-bar-container">
-        <div className="side-bar-header">
-          <h2>Name Program</h2>
-        </div>
-          <div className="side-bar-info">
-            <div className="sub-info"><i class="fas fa-anchor"></i>Threads</div>
-            <div className="sub-info"><i class="fas fa-comment-dots"></i>ALL DMs</div>
-            <div className="sub-info"><i class="fas fa-at"></i>Mentions and reactions</div>
-            <div className="sub-info"><i class="far fa-bookmark"></i>Saved Items</div>
-            <div className="sub-info"><i class="fas fa-angle-double-right"></i>More</div>
-          </div>
-          <div className="side-bar-channels">
-          <Channels />
-          </div>
-          <div className="side-bar-messages">
-          <Messages />
-          </div>
-          <div className="side-bar-logout">
-          <Logout />
-          </div>
-        
-      </div>
+      <Menu />
       <Switch>
         <Route path='/client/channels/:id'>
-          <div className='channel-details'>
           <ChannelDetails />
-          </div>
         </Route>
-        <Route path='/client/messages'>
-          <h1>Hello Messages</h1>
+        <Route path='/client/messages/:id'>
+          <ContactDetails />
         </Route>
       </Switch>
     </div>
