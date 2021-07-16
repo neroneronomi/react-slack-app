902252b2b7f74a3c85dc70151583cdc082f80c59
Oliver Nero <oliver_nero11@icloud.com>
2021-07-16 20:37:04 +0800

    testing

--- src/components/Channels/ChannelFeed.jsx
+++ src/components/Channels/ChannelFeed.jsx
@@ -5,9 +5,9 @@ const ChannelFeed = ({ headers, id }) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   const messagesEndRef = useRef(null)
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "auto" })
-  }
+  // const scrollToBottom = () => {
+  //   messagesEndRef.current?.scrollIntoView({ behavior: "auto" })
+  // }
   useEffect(() => {
     const requestOptions = {
       method: "GET",
@@ -32,7 +32,7 @@ const ChannelFeed = ({ headers, id }) => {
         setIsPending(false);
         setError(error);
       });
-      scrollToBottom()
+      // scrollToBottom()
     // eslint-disable-next-line
   }, [messages]);
   return (
