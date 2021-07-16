4d7d24e9081c50d635154eaf1d68eaee897ca21c
Steven Choy <stevenchoy0915@gmail.com>
2021-07-16 20:49:34 +0800

    add test for messages.js

--- src/components/Messages/FindUserModal.jsx
+++ src/components/Messages/FindUserModal.jsx
@@ -6,13 +6,13 @@ import FindUser from './FindUser';
 const FindUserModal = () => {
   return (
     <Popup
-    trigger={<button type='button'><i className="fas fa-plus"></i></button>}
+    trigger={<button title='button' type='button'><i className="fas fa-plus"></i></button>}
     modal
     nested
   >
     {close => (
       <div className="modal">
-        <button className="close" onClick={close}>
+        <button className="close" title="close" onClick={close}>
           &times;
         </button>
         <div className="header">Add to contacts.</div>
